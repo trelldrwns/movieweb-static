765d512f61a2779852c6274c990afe0f08a4c695
Trell <114397001+trelldrwns@users.noreply.github.com>
2023-07-11 21:33:05 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://movie-web.trellyt357.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNzliNDkyZjdjZjBiZmVjYTE1Njg1OWI5YTM0YTkzMyIsInN1YiI6IjY0YWM1MzliYjY4NmI5MDE1MDExMDBmMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NvWToc_fwaIEy9a15gDOUgyNxGD7idi1Ybscb01-lfw"
+};
